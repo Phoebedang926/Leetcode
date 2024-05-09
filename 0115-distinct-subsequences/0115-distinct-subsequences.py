b0f7c60815abca08da6042945be57748feac93b4
Phoebe Dang <143676255+Phoebedang926@users.noreply.github.com>
2024-05-10 00:03:27 +1000

    Time: 189 ms (87.25%), Space: 17.1 MB (88.68%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,31 +1,32 @@
-class Solution:
-    def numDistinct(self, s: str, t: str) -> int:
-        cache = {}
-        def dp(i,j):
-            if j == len(t):
-                return 1
-            if i == len(s):
-                return 0
-            if (i,j) in cache:
-                return cache[(i,j)]
-            
-            if s[i] == t[j]:
-                cache[(i,j)] = dp(i+1,j+1) + dp(i+1,j)
-            else:
-                cache[(i,j)] = dp(i+1,j)
-            return cache[(i,j)]
-        return dp(0,0)
 # class Solution:
 #     def numDistinct(self, s: str, t: str) -> int:
-#         N, M = len(s), len(t)
-#         dp = [0] * (N+1)
-#         dp[0] = 1
-#         for m in range(M):
-#             curdp = [0] * (N+1)
-#             for n in range(N):
-#                 if s[n] == t[m]:
-#                     curdp[n+1] += dp[n]
-#                 curdp[n+1] += curdp[n]
-#             dp = curdp
-#         return dp[N]
-        
+#         cache = {}
+#         def dp(i,j):
+#             if j == len(t):
+#                 return 1
+#             if i == len(s):
+#                 return 0
+#             if (i,j) in cache:
+#                 return cache[(i,j)]
+            
+#             if s[i] == t[j]:
+#                 cache[(i,j)] = dp(i+1,j+1) + dp(i+1,j)
+#             else:
+#                 cache[(i,j)] = dp(i+1,j)
+#             return cache[(i,j)]
+#         return dp(0,0)
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        N, M = len(s), len(t)
+        dp = [0] * (M+1) 
+        dp[M] = 1
+        for n in range(N-1,-1, -1):
+            curdp = [0] * (M+1)
+            curdp[M] = 1
+            for m in range(M-1,-1,-1):
+                if s[n] == t[m]:
+                    curdp[m] = dp[m+1] + dp[m]
+                else:
+                    curdp[m] = dp[m]
+            dp = curdp
+        return dp[0]
