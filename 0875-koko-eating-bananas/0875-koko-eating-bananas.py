ba65a9d55b60ff50101c56721cd859b3ab4f6afa
Phoebe Dang <143676255+Phoebedang926@users.noreply.github.com>
2024-06-12 18:56:34 +1000

    Time: 283 ms (31.90%), Space: 18.1 MB (84.32%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,7 +1,7 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l, r = 1, max(piles)
-        res = r
+        # res = r
 
         while l<=r:
             k = (l+r)//2
@@ -9,12 +9,12 @@ class Solution:
             for p in piles:
                 time += math.ceil(p/k)
             if time <= h:
-                res = min(r, k)
+                # res = min(r, k)
                 r = k-1
             else:
                 l = k +1
             
-        return res
+        return l
                 
  
 
