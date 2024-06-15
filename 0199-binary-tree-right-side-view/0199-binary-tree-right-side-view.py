1a0c42af6cd6834a0290c18df11dd74cdd444d1e
Phoebe Dang <143676255+Phoebedang926@users.noreply.github.com>
2024-06-16 01:48:35 +1000

    Time: 38 ms (45.50%), Space: 16.6 MB (34.95%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,14 +7,11 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         queue = deque()
-        res =[]
-               
+        res = []
         if root:
             queue.append(root)
-            
-        while len(queue) > 0:
+        while len(queue)> 0:
             res.append(queue[-1].val)
-            
             for i in range(len(queue)):
                 node = queue.popleft()
                 if node.left:
@@ -22,22 +19,4 @@ class Solution:
                 if node.right:
                     queue.append(node.right)
         return res
-     
-#         queue = deque()
-#         res =[]
-               
-#         if root:
-#             queue.append(root)
-            
-#         while len(queue) > 0:
-#             level = []
-#             for i in range(len(queue)):
-#                 node = queue.popleft()
-#                 if node:
-#                     level.append(node.val)
-#                     queue.append(node.left)
-#                     queue.append(node.right)
-#             if level:
-#                 res.append(level[-1])
-#         return res
                 
