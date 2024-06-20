06f6666c1e57c3ae84510aad54b41b96bbb1e71e
Phoebe Dang <143676255+Phoebedang926@users.noreply.github.com>
2024-06-21 01:15:50 +1000

    Time: 638 ms (16.45%), Space: 22.8 MB (78.43%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,26 +1,13 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         minheap = []
-        for x, y in points:
-            dis = x**2 + y**2
+        for x,y in points:
+            dis = sqrt(x**2 + y**2)
             minheap.append([dis,x,y])
         heapq.heapify(minheap)
-        
         res = []
-        while k > 0:
+        for i in range(k):
             dis,x,y = heapq.heappop(minheap)
             res.append([x,y])
-            k -=1
         return res
-
-
-
-        # dis = {}
-        # for i in range(len(points)):
-        #     dis[i] = points[i][0]**2 + points[i][1]**2  # Corrected exponentiation operator
-
-        # sorted_dict = sorted(dis.items(), key=lambda x: x[1])
-        # keys_subset = [key for key, _ in sorted_dict[:k]]  # Extract keys from sorted dictionary
-
-        # result = [points[key] for key in keys_subset]  # Extract corresponding points
-        # return result
+            
